02c659d564c936669ff1e9513e5e7d41cbd78b41
José Smutný <smutnjo@gmail.com>
2021-03-06 18:40:42 +0100

    Add styles using enqueue

--- functions.php
+++ functions.php
@@ -13,5 +13,14 @@ register_nav_menus(array(
     'pages-home'=> __('Pages Menu')
 ));
 
+
+add_action( 'wp_enqueue_scripts', 'front_page_style' );
+
+function front_page_style(){
+    if ( is_front_page() || is_home()) {
+        wp_enqueue_style( 'home', get_stylesheet_directory_uri() . '/assets/css/home.min.css' );
+    }
+}
+
 add_filter('show_admin_bar', '__return_false');
 ?>
