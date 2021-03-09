f8d990d28a3d50eb5182299aec940f9e8e5b1f26
José Smutný <smutnjo@gmail.com>
2021-03-09 01:18:19 +0100

    Style 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,35 @@
+<?php 
+
+$lang = substr(get_locale(), 0, 2);
+
+
+if( $lang == "cs" ){
+    $title = "Jejda";
+    $content = "Zdá se, že tato stránka neexistuje.</p><p>Pokud by existovat měla, napiš prosím na <a href='mailto:admin@zivastena.eu'>admin@zivastena.eu</a></p><p>V každem případě by tyto odkazy už měli fungovat v pořádku:";
+} 
+else if( $lang == "es"){
+    $title = "404? En serio?";
+    $content = "Parece que esta página no existe.</p><p>Si crees que no debería ser así,contáctanos en <a href='mailto:admin@zivastena.eu'>admin@zivastena.eu</a></p><p>De cualquier manera, aquí hay un par de links que deberían funcionar:";
+} 
+else {
+    $title = "Whoopsie daisy!";
+    $content = "It seems that the page you're looking for isn't here.</p><p>If you think it should, please drop a message to <a href='mailto:admin@zivastena.eu'>admin@zivastena.eu</a></p><p>In any case, these links should work OK:";
+}
+
+
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nenalézená stránka</title>
+    <?php wp_head(); ?>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
-    <h1>Whoopsy daisy</h1>
+    <h1><?php echo $title; ?></h1>
+    <p class="content"><?php echo $content ?></p>
+    <?php wp_nav_menu(array('theme_location' => 'pages-home','container_id'=>'footer')); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -15,16 +15,21 @@ register_nav_menus(array(
 ));
 
 
-add_action( 'wp_enqueue_scripts', 'front_page_style' );
+add_action( 'wp_enqueue_scripts', 'styles' );
 add_action( 'wp_enqueue_scripts', 'live_js');
 
-function front_page_style(){
+function styles(){
     if ( is_front_page() || is_home()) {
         wp_enqueue_style( 'home', get_stylesheet_directory_uri() . '/assets/css/home.min.css' );
     }
     if ( is_page_template("category-challenge.php" )){
         wp_enqueue_style('challenge', get_stylesheet_directory_uri() . '/assets/css/challenge.min.css' ); 
     }
+
+    if( is_404() ){
+        wp_enqueue_style( '404', get_stylesheet_directory_uri() . '/assets/css/404.min.css');
+    }
+
 }
 
 function live_js(){
