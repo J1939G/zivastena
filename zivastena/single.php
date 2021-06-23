920b9740efd78adbad8219d841029ebc4775a922
José Smutný <smutnjo@gmail.com>
2021-06-23 12:58:34 +0200

    Move head tags to wp_head

--- zivastena/single.php
+++ zivastena/single.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 the_post();
 $assets = get_stylesheet_directory_uri();
@@ -7,16 +7,15 @@ $assets = get_stylesheet_directory_uri();
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
     <title><?php the_title(); ?></title>
-	<script src="<?php echo $assets.'/assets/scripts/live.js' ?>"></script>
-	<link rel="stylesheet" href="<?php echo $assets.'/assets/css/category' ?>.css">
+    <?php wp_head(); ?>
 </head>
 <body>
     <h1><?php the_title(); ?></h1>
-    <div id="content" data-featherlight=".lbox">
-        <?php 
-            the_content("", FALSE);
+    <div class="content">
+        <?php
+            the_content("", false);
             get_footer();
         ?>
