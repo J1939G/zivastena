bd635c59bb5173ecd8f45479811022b42ec993af
José Smutný <smutnjo@gmail.com>
2021-06-23 12:55:28 +0200

    Reformat file

--- zivastena/functions.php
+++ zivastena/functions.php
@@ -1,43 +1,56 @@
-<?php 
+<?php
 function wpit_resources()
 {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'wpit_resources');
 
-add_theme_support( 'post-thumbnails' ); 
-add_theme_support( 'html5', array( 'gallery', 'caption' ) );
+add_theme_support('post-thumbnails');
+add_theme_support('html5', array( 'gallery', 'caption' ));
 
 register_nav_menus(array(
     'main' => __( 'Main navigation'),
     'footer'=> __('Footer menu')
 ));
 
-add_action( 'wp_enqueue_scripts', 'styles' );
-add_action( 'wp_enqueue_scripts', 'scripts' );
-add_action( 'wp_enqueue_scripts', 'live_js');
 
-function enqueue_style($filename){
-    wp_enqueue_style( $filename, get_stylesheet_directory_uri() . '/assets/css/' . $filename . '.min.css' );
+function enqueue_style($filename)
+{
+    wp_enqueue_style($filename, get_stylesheet_directory_uri() . '/assets/css/' . $filename . '.min.css');
 }
 
-function styles(){
-    if ( is_front_page() || is_home()) 
+function styles()
+{
+    if (is_front_page() || is_home()) {
         enqueue_style('home');
-    if( is_404() )
+    }
+    if (is_404()) {
         enqueue_style('404');
-    if( is_category() )
+    }
+    if (is_category()) {
         enqueue_style('category');
-    if( is_page() ) 
-        enqueue_style('page'); 
+    }
+    if (is_single()) {
+        enqueue_style('single');
+    }
+    if (is_page()) {
+        enqueue_style('page');
+    }
 }
 
+add_action('wp_enqueue_scripts', 'styles');
+add_action('wp_enqueue_scripts', 'scripts');
+add_action('wp_enqueue_scripts', 'live_js');
 
 
-function mobile_theme_color() {
+function mobile_theme_color()
+{
     echo '<meta name="theme-color" content="#404040" />';
+    echo '<meta charset="<?php bloginfo( \'charset\' ); ?>" />';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1" />';
 }
-add_action( 'wp_head', 'mobile_theme_color');
+
+add_action('wp_head', 'mobile_theme_color');
 
 
 function scripts()
@@ -49,8 +62,9 @@ function scripts()
     }
 }
 
-function live_js(){
-    wp_enqueue_script( 'live', get_stylesheet_directory_uri() . '/assets/scripts/live.js', NULL, TRUE);
+function live_js()
+{
+    wp_enqueue_script('live', get_stylesheet_directory_uri() . '/assets/scripts/live.js', null, true);
 }
 
 add_filter('show_admin_bar', '__return_false');
@@ -58,32 +72,32 @@ add_filter('show_admin_bar', '__return_false');
 // clean wp_head ------------------------------------------------------------
 
 // Removes the wlwmanifest link
-remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action('wp_head', 'wlwmanifest_link');
 // Removes the RSD link
-remove_action( 'wp_head', 'rsd_link' );
+remove_action('wp_head', 'rsd_link');
 // Removes the WP shortlink
-remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 // Removes the canonical links
-remove_action( 'wp_head', 'rel_canonical' );
+remove_action('wp_head', 'rel_canonical');
 // Removes the links to the extra feeds such as category feeds
-remove_action( 'wp_head', 'feed_links_extra', 3 ); 
+remove_action('wp_head', 'feed_links_extra', 3);
 // Removes links to the general feeds: Post and Comment Feed
-remove_action( 'wp_head', 'feed_links', 2 ); 
+remove_action('wp_head', 'feed_links', 2);
 // Removes the index link
-remove_action( 'wp_head', 'index_rel_link' ); 
+remove_action('wp_head', 'index_rel_link');
 // Removes the prev link
-remove_action( 'wp_head', 'parent_post_rel_link' ); 
+remove_action('wp_head', 'parent_post_rel_link');
 // Removes the start link
-remove_action( 'wp_head', 'start_post_rel_link' ); 
+remove_action('wp_head', 'start_post_rel_link');
 // Removes the relational links for the posts adjacent to the current post
 //remove_action( 'wp_head', 'adjacent_posts_rel_link' );
 //remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' );
 
 // Removes the WordPress version i.e. -
-remove_action( 'wp_head', 'wp_generator' );
+remove_action('wp_head', 'wp_generator');
 
-// remove emojis 
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+// remove emojis
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
