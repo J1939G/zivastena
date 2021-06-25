40818b2fb933cebeacb3815a7c14429e7118211c
José Smutný <smutnjo@gmail.com>
2021-06-25 08:57:56 +0200

    Add localized links to home

--- zivastena/header.php
+++ zivastena/header.php
@@ -3,6 +3,6 @@
 		<span class="top"></span>
 		<span class="bottom"></span>
 	</button>
-	<h1 class="title-wrapper"><a href='/' class='title'><?php echo get_bloginfo('name') ?></a></h1>
+	<h1 class="title-wrapper"><a href='<?php echo pll_home_url() ?>' class='title'><?php echo get_bloginfo('name') ?></a></h1>
 	<?php wp_nav_menu(array('theme_location' => 'main','container_class'=>'links')); ?>
 </nav>
